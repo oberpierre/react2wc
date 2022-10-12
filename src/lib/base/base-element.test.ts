4d7ace5ee6a7bf63bb6871bf68e545a65b6ba1ad
Pierre Obermaier <pierre.obermaier@googlemail.com>
2022-10-12 19:32:40 +0800

    t Testing error cases

--- src/lib/base/base-element.test.ts
+++ src/lib/base/base-element.test.ts
@@ -37,11 +37,11 @@ const asHtmlElement: (shadowRoot: ShadowRoot | null) => HTMLElement = (
 beforeAll(() => {
   customElements.define('hello-world', HelloWorldWebcomponent);
 });
-beforeEach(() => {
+afterEach(() => {
   document
     .querySelectorAll('hello-world')
     .forEach((child) => document.body.removeChild(child));
-  jest.clearAllMocks();
+  jest.restoreAllMocks();
 });
 
 describe('BaseElement', () => {
@@ -81,6 +81,109 @@ describe('BaseElement', () => {
           name: 'Hello Bar',
         })
       ).toBeInTheDocument()
+    );
+  });
+
+  it('should throw error if dependencies cannot be loaded', async () => {
+    jest
+      .spyOn(HelloWorldWebcomponent.prototype, 'getComponent')
+      .mockRejectedValue('404: Not Found');
+    const errorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementationOnce(jest.fn);
+    const test = new HelloWorldWebcomponent();
+    test.setAttribute('name', 'Foo');
+
+    document.body.appendChild(test);
+
+    await waitFor(() => expect(errorSpy).toHaveBeenCalledTimes(1));
+    expect(errorSpy).toHaveBeenLastCalledWith('404: Not Found');
+  });
+  it('should throw error if shadow root is not available in render', async () => {
+    const errorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementationOnce(jest.fn);
+    const test = new HelloWorldWebcomponent();
+    jest.spyOn(test, 'shadowRoot', 'get').mockReturnValue(null);
+
+    document.body.appendChild(test);
+
+    await waitFor(() => expect(errorSpy).toHaveBeenCalledTimes(1));
+    expect(errorSpy).toHaveBeenLastCalledWith(
+      new Error(
+        'Unexpected error while rendering component. ShadowRoot unavailable.'
+      )
+    );
+  });
+  it('should throw error if component is undefined after loading', async () => {
+    jest
+      .spyOn(HelloWorldWebcomponent.prototype, 'getComponent')
+      .mockResolvedValue(undefined as unknown as HelloWorldComp);
+    const errorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementationOnce(jest.fn);
+    const test = new HelloWorldWebcomponent();
+    test.setAttribute('name', 'Foo');
+
+    document.body.appendChild(test);
+
+    await waitFor(() => expect(errorSpy).toHaveBeenCalledTimes(1));
+    expect(errorSpy).toHaveBeenLastCalledWith(
+      new Error(
+        "Unexpected error while rendering component. Dependencies couldn't be loaded."
+      )
+    );
+  });
+  it('should throw error if component is undefined after loading', async () => {
+    jest
+      .spyOn(HelloWorldWebcomponent.prototype, 'getComponent')
+      .mockResolvedValue(undefined as unknown as HelloWorldComp);
+    const errorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementationOnce(jest.fn);
+    const test = new HelloWorldWebcomponent();
+    test.setAttribute('name', 'Foo');
+
+    document.body.appendChild(test);
+
+    await waitFor(() => expect(errorSpy).toHaveBeenCalledTimes(1));
+    expect(errorSpy).toHaveBeenLastCalledWith(
+      new Error(
+        "Unexpected error while rendering component. Dependencies couldn't be loaded."
+      )
+    );
+  });
+  it('should throw error if component is undefined upon rerendering', async () => {
+    const errorSpy = jest
+      .spyOn(console, 'error')
+      .mockImplementationOnce(jest.fn);
+    const test = new HelloWorldWebcomponent();
+    const _setAttribute = test.setAttribute.bind(test);
+    test.setAttribute('name', 'Foo');
+    jest
+      .spyOn(test, 'setAttribute')
+      // .mockImplementationOnce(_setAttribute)
+      .mockImplementationOnce((name, value) => {
+        (test as unknown as { component: unknown }).component = undefined;
+        _setAttribute(name, value);
+      })
+      .mockImplementation(_setAttribute);
+
+    document.body.appendChild(test);
+
+    await waitFor(() =>
+      expect(
+        getByRole(asHtmlElement(test.shadowRoot), 'heading', {
+          name: 'Hello Foo',
+        })
+      ).toBeInTheDocument()
+    );
+
+    test.setAttribute('name', 'Bar');
+
+    await waitFor(() => expect(errorSpy).toHaveBeenCalledTimes(1));
+    expect(errorSpy).toHaveBeenLastCalledWith(
+      new Error('Unexpected error while rerendering component.')
     );
   });
 });
