a26d61ec844e4080f05b47b64916e870f0efc572
Pierre Obermaier <pierre.obermaier@googlemail.com>
2022-10-12 16:13:41 +0800

    F Creating base element for transformation

--- src/lib/base/base-element.test.ts
+++ src/lib/base/base-element.test.ts
@@ -1,7 +1,39 @@
+import { getByRole, waitFor } from '@testing-library/dom';
+import * as React from 'react';
 import BaseElement from './base-element';
 
+const HelloWorld: React.FunctionComponent = () => {
+  return React.createElement('h1', null, ['Hello World']);
+};
+
+type HelloWorldComp = typeof HelloWorld;
+
+class HelloWorldWebcomponent extends BaseElement<HelloWorldComp> {
+  getComponent(): Promise<React.FunctionComponent> {
+    return Promise.resolve(HelloWorld);
+  }
+}
+
+const asHtmlElement: (shadowRoot: ShadowRoot | null) => HTMLElement = (
+  root
+) => {
+  return root as unknown as HTMLElement;
+};
+
 describe('BaseElement', () => {
-  it('should be a function constructor', () => {
-    expect(BaseElement).toBeInstanceOf(Function);
+  it('should render Hello World', async () => {
+    customElements.define('hello-world', HelloWorldWebcomponent);
+
+    const test = new HelloWorldWebcomponent();
+
+    document.body.appendChild(test);
+
+    await waitFor(() =>
+      expect(
+        getByRole(asHtmlElement(test.shadowRoot), 'heading', {
+          name: 'Hello World',
+        })
+      ).toBeInTheDocument()
+    );
   });
 });
--- src/lib/base/base-element.ts
+++ src/lib/base/base-element.ts
@@ -1,3 +1,58 @@
-abstract class BaseElement {}
+import { html, LitElement, TemplateResult } from 'lit';
+import {
+  Attributes,
+  ComponentProps,
+  ComponentType,
+  ReactElement,
+  ReactNode,
+} from 'react';
+import { Root, RootOptions } from 'react-dom/client';
+
+type ReactAbstraction = {
+  createElement<P extends Record<string, unknown>>(
+    type: ComponentType<P> | string,
+    props?: (Attributes & P) | null,
+    ...children: ReactNode[]
+  ): ReactElement<P>;
+};
+
+type ReactDomAbstraction = {
+  createRoot: (
+    container: Element | DocumentFragment,
+    options?: RootOptions
+  ) => Root;
+};
+
+abstract class BaseElement<
+  T extends ComponentType<P>,
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  P extends {} = ComponentProps<T>
+> extends LitElement {
+  protected component: Promise<T>;
+  protected react: Promise<ReactAbstraction>;
+  protected reactDom: Promise<ReactDomAbstraction>;
+
+  constructor() {
+    super();
+    this.component = this.getComponent();
+    this.react = import('react');
+    this.reactDom = import('react-dom/client');
+  }
+
+  render(): TemplateResult {
+    Promise.all([this.component, this.react, this.reactDom])
+      .then(([component, react, reactDom]) => {
+        if (!this.shadowRoot) {
+          throw new Error("Couldn't render component. ShadowRoot unavailable.");
+        }
+        const root = reactDom.createRoot(this.shadowRoot);
+        root.render(react.createElement(component, null, []));
+      })
+      .catch(console.error);
+    return html``;
+  }
+
+  abstract getComponent(): Promise<T>;
+}
 
 export default BaseElement;
